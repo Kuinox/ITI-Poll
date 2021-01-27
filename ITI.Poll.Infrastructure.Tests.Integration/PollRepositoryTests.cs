6d9f923de78eabe7cf916a7837e4c7a28026d354
Pierre Viara <pierre.viara@outlook.com>
2021-01-27 19:49:05 +0100

    test(poll-repo): check whether a poll gets created correctly

---
+++ ITI.Poll.Infrastructure.Tests.Integration/PollRepositoryTests.cs
@@ -0,0 +1,68 @@
+using FluentAssertions;
+using ITI.Poll.Tests;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace ITI.Poll.Infrastructure.Tests.Integration
+{
+    [TestFixture]
+    public class PollRepositoryTests
+    {
+        [Test]
+        public async Task create_poll()
+        {
+            using(PollContext pollContext = TestHelpers.CreatePollContext())
+            {
+                var pollContextAccessor = new PollContextAccessor(pollContext);
+                var userRepository = new UserRepository(pollContextAccessor);
+                var sut = new PollRepository(pollContextAccessor);
+                var userService = TestHelpers.UserService;
+
+                // create the user that'll later be the poll author
+                var email = $"{Guid.NewGuid()}@test.org";
+                var nickname = $"Test-{Guid.NewGuid()}";
+                await userService.CreateUser(
+                    userRepository,
+                    email,
+                    nickname,
+                    "test-hash"
+                );
+                var author = await userService.FindByNickname(userRepository, nickname);
+
+                // create the guests that'll be used to create the poll
+                var guest1 = await TestHelpers.UserService.CreateUser(
+                    userRepository,
+                    $"{Guid.NewGuid()}@test.org",
+                    $"Test-{Guid.NewGuid()}",
+                    "test-hash"
+                );
+                var guest2 = await TestHelpers.UserService.CreateUser(
+                    userRepository,
+                    $"{Guid.NewGuid()}@test.org",
+                    $"Test-{Guid.NewGuid()}",
+                    "test-hash"
+                );
+
+                // create the poll using the previously created author
+                var poll = new Model.Poll(0, author.Value.UserId, "question?", false);
+                poll.AddGuest(guest1.Value.UserId, await sut.GetNoProposal());
+                poll.AddGuest(guest2.Value.UserId, await sut.GetNoProposal());
+                poll.AddProposal("P1");
+                poll.AddProposal("P2");
+
+                var result = await sut.Create(poll);
+                result.IsSuccess.Should().BeTrue();
+                poll.PollId.Should().NotBe(0);
+
+                var createdPoll = await sut.FindById(poll.PollId);
+                createdPoll.Value.PollId.Should().Be(poll.PollId);
+                
+                // clean up the whole thing
+                await TestHelpers.UserService.DeleteUser(pollContext, userRepository, sut, author.Value.UserId);
+                await TestHelpers.UserService.DeleteUser(pollContext, userRepository, sut, guest1.Value.UserId);
+                await TestHelpers.UserService.DeleteUser(pollContext, userRepository, sut, guest2.Value.UserId);
+            }
+        }
+    }
+}
