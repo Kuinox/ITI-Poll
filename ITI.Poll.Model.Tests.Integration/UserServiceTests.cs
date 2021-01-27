7f0768938ebed0b2729876f5f7e1e27eb418bd3a
Kuinox <nvandeginste@gmail.com>
2021-01-27 16:18:04 +0100

    1 test !

--- ITI.Poll.Model.Tests.Integration/UserServiceTests.cs
+++ ITI.Poll.Model.Tests.Integration/UserServiceTests.cs
@@ -7,30 +7,55 @@ using NUnit.Framework;
 
 namespace ITI.Poll.Model.Tests.Integration
 {
-    public class UserServiceTests
-    {
-        [Test]
-        public async Task create_user()
-        {
-            using(PollContext pollContext = TestHelpers.CreatePollContext())
-            {
-                PollContextAccessor pollContextAccessor = new PollContextAccessor(pollContext);
-                UserRepository userRepository = new UserRepository(pollContextAccessor);
-                string email = $"test-{Guid.NewGuid()}@test.fr";
-                string nickname = $"Test-{Guid.NewGuid()}";
+	public class UserServiceTests
+	{
+		[Test]
+		public async Task create_user()
+		{
+			using (PollContext pollContext = TestHelpers.CreatePollContext())
+			{
+				PollContextAccessor pollContextAccessor = new PollContextAccessor(pollContext);
+				UserRepository userRepository = new UserRepository(pollContextAccessor);
+				string email = $"test-{Guid.NewGuid()}@test.fr";
+				string nickname = $"Test-{Guid.NewGuid()}";
 
-                Result<User> user = await TestHelpers.UserService.CreateUser(userRepository, email, nickname, "validpassword");
+				Result<User> user = await TestHelpers.UserService.CreateUser(userRepository, email, nickname, "validpassword");
 
-                user.IsSuccess.Should().BeTrue();
-                user.Value.Email.Should().Be(email);
-                user.Value.Nickname.Should().Be(nickname);
+				user.IsSuccess.Should().BeTrue();
+				user.Value.Email.Should().Be(email);
+				user.Value.Nickname.Should().Be(nickname);
 
-                user = await TestHelpers.UserService.FindByNickname(userRepository, nickname);
-                user.IsSuccess.Should().BeTrue();
+				user = await TestHelpers.UserService.FindByNickname(userRepository, nickname);
+				user.IsSuccess.Should().BeTrue();
 
-                PollRepository pollRepository = new PollRepository(pollContextAccessor);
-                await TestHelpers.UserService.DeleteUser(pollContext, userRepository, pollRepository, user.Value.UserId);
-            }
-        }
-    }
+				PollRepository pollRepository = new PollRepository(pollContextAccessor);
+				await TestHelpers.UserService.DeleteUser(pollContext, userRepository, pollRepository, user.Value.UserId);
+			}
+		}
+
+		[Test]
+		public async Task deleted_user_cannot_create_poll() // Nicolas
+		{
+			using (PollContext pollContext = TestHelpers.CreatePollContext())
+			{
+				PollContextAccessor pollContextAccessor = new PollContextAccessor(pollContext);
+				UserRepository userRepository = new UserRepository(pollContextAccessor);
+				PollRepository pollRepository = new PollRepository(pollContextAccessor);
+
+				Result<User> user = await TestHelpers.UserService.CreateUser(userRepository, $"test-{Guid.NewGuid()}@test.fr", $"Test-{Guid.NewGuid()}", "validpassword");
+				Result<User> userGuest = await TestHelpers.UserService.CreateUser(userRepository, $"test-{Guid.NewGuid()}@test.fr", $"Test-{Guid.NewGuid()}", "validpassword");
+				Result res = await TestHelpers.UserService.DeleteUser(pollContext, userRepository, pollRepository, user.Value.UserId);
+				res.IsSuccess.Should().BeTrue();
+
+				Result<Poll> pollRes = await TestHelpers.PollService.CreatePoll(pollContext, pollRepository, userRepository, new NewPollDto()
+				{
+					AuthorId = user.Value.UserId,
+					Question = "A question",
+					Proposals = new string[] { "Proposal A", "Proposal B" },
+					GuestNicknames = new string[] { userGuest.Value.Nickname }
+				});
+				pollRes.Error.Should().Be(Errors.AccountDeleted);
+			}
+		}
+	}
 }
--- ITI.Poll.Model/PollService.cs
+++ ITI.Poll.Model/PollService.cs
@@ -5,122 +5,123 @@ using System.Threading.Tasks;
 
 namespace ITI.Poll.Model
 {
-    public sealed class PollService : IPollService
-    {
-        static readonly string InvalidAuthorIdMsg = "The author id is invalid.";
-        static readonly string NotEnoughGuestsMsg = "At least 1 guest is mandatory.";
-        static readonly string AuthorCannotBeGuestMsg = "The author of a poll can not be a guest of this poll.";
-        static readonly string EmptyQuestionMsg = "The question must be not null nor white space.";
-        static readonly string NotEnoughProposalsMsg = "At least 2 proposals are mandatory.";
-        static readonly string EmptyProposalMsg = "The proposals must be not null nor white space.";
-        static readonly string ForbiddenMsg = "Forbidden";
+	public sealed class PollService : IPollService
+	{
+		static readonly string InvalidAuthorIdMsg = "The author id is invalid.";
+		static readonly string NotEnoughGuestsMsg = "At least 1 guest is mandatory.";
+		static readonly string AuthorCannotBeGuestMsg = "The author of a poll can not be a guest of this poll.";
+		static readonly string EmptyQuestionMsg = "The question must be not null nor white space.";
+		static readonly string NotEnoughProposalsMsg = "At least 2 proposals are mandatory.";
+		static readonly string EmptyProposalMsg = "The proposals must be not null nor white space.";
+		static readonly string ForbiddenMsg = "Forbidden";
+		static readonly string AuthorAccountDeletedMsg = "The author account is deleted and cannot create a new poll.";
+		public async Task<Result<Poll>> CreatePoll(IUnitOfWork unitOfWork, IPollRepository pollRepository, IUserRepository userRepository, NewPollDto newPollInfo)
+		{
+			Result<User> author = await userRepository.FindById(newPollInfo.AuthorId);
+			if (!author.IsSuccess) return Result.Map<Poll>(author);
+			Result validation = Validate(newPollInfo, author.Value);
+			if (!validation.IsSuccess) return Result.Map<Poll>(validation);
 
-        public async Task<Result<Poll>> CreatePoll(IUnitOfWork unitOfWork, IPollRepository pollRepository, IUserRepository userRepository, NewPollDto newPollInfo)
-        {
-            Result<User> author = await userRepository.FindById(newPollInfo.AuthorId);
-            if (!author.IsSuccess) return Result.Map<Poll>(author);
+			Poll poll = new Poll(0, newPollInfo.AuthorId, newPollInfo.Question, false);
+			Result pollCreation = await pollRepository.Create(poll);
+			if (!pollCreation.IsSuccess) return Result.Map<Poll>(pollCreation);
 
-            Result validation = Validate(newPollInfo, author.Value.Nickname);
-            if (!validation.IsSuccess) return Result.Map<Poll>(validation);
+			foreach (string proposalText in newPollInfo.Proposals) poll.AddProposal(proposalText);
+			Proposal noProposal = await pollRepository.GetNoProposal();
+			foreach (string guestNickname in newPollInfo.GuestNicknames)
+			{
+				Result<User> guest = await userRepository.FindByNickname(guestNickname);
+				if (!guest.IsSuccess) return Result.Map<Poll>(guest);
+				poll.AddGuest(guest.Value.UserId, noProposal);
+			}
 
-            Poll poll = new Poll(0, newPollInfo.AuthorId, newPollInfo.Question, false);
-            Result pollCreation = await pollRepository.Create(poll);
-            if (!pollCreation.IsSuccess) return Result.Map<Poll>(pollCreation);
+			await unitOfWork.SaveChanges();
 
-            foreach (string proposalText in newPollInfo.Proposals) poll.AddProposal(proposalText);
-            Proposal noProposal = await pollRepository.GetNoProposal();
-            foreach (string guestNickname in newPollInfo.GuestNicknames)
-            {
-                Result<User> guest = await userRepository.FindByNickname(guestNickname);
-                if (!guest.IsSuccess) return Result.Map<Poll>(guest);
-                poll.AddGuest(guest.Value.UserId, noProposal);
-            }
+			return Result.CreateSuccess(poll);
+		}
 
-            await unitOfWork.SaveChanges();
+		Result Validate(NewPollDto newPollInfo, User author)
+		{
+			if (author.IsDeleted)
+				return Result.CreateError(Errors.AccountDeleted, AuthorAccountDeletedMsg);
+			if (newPollInfo.AuthorId < 16)
+				return Result.CreateError(Errors.InvalidAuthorId, InvalidAuthorIdMsg);
+			if (newPollInfo.GuestNicknames == null || newPollInfo.GuestNicknames.Length < 1)
+				return Result.CreateError(Errors.NotEnoughGuests, NotEnoughGuestsMsg);
+			if (newPollInfo.GuestNicknames.Select(n => n.ToLowerInvariant()).Contains(author.Nickname.ToLowerInvariant()))
+				return Result.CreateError(Errors.AuthorCannotBeGuest, AuthorCannotBeGuestMsg);
+			if (string.IsNullOrWhiteSpace(newPollInfo.Question))
+				return Result.CreateError(Errors.EmptyQuestion, EmptyQuestionMsg);
+			if (newPollInfo.Proposals == null || newPollInfo.Proposals.Length < 2)
+				return Result.CreateError(Errors.NotEnoughProposals, NotEnoughProposalsMsg);
+			if (newPollInfo.Proposals.Any(p => string.IsNullOrWhiteSpace(p)))
+				return Result.CreateError(Errors.EmptyProposal, EmptyProposalMsg);
 
-            return Result.CreateSuccess(poll);
-        }
+			return Result.CreateSuccess();
+		}
 
-        Result Validate(NewPollDto newPollInfo, string authorNickname)
-        {
-            if (newPollInfo.AuthorId < 16)
-                return Result.CreateError(Errors.InvalidAuthorId, InvalidAuthorIdMsg);
-            if (newPollInfo.GuestNicknames == null || newPollInfo.GuestNicknames.Length < 1)
-                return Result.CreateError(Errors.NotEnoughGuests, NotEnoughGuestsMsg);
-            if (newPollInfo.GuestNicknames.Select(n => n.ToLowerInvariant()).Contains(authorNickname.ToLowerInvariant()))
-                return Result.CreateError(Errors.AuthorCannotBeGuest, AuthorCannotBeGuestMsg);
-            if (string.IsNullOrWhiteSpace(newPollInfo.Question))
-                return Result.CreateError(Errors.EmptyQuestion, EmptyQuestionMsg);
-            if (newPollInfo.Proposals == null || newPollInfo.Proposals.Length < 2)
-                return Result.CreateError(Errors.NotEnoughProposals, NotEnoughProposalsMsg);
-            if (newPollInfo.Proposals.Any(p => string.IsNullOrWhiteSpace(p)))
-                return Result.CreateError(Errors.EmptyProposal, EmptyProposalMsg);
+		public async Task<Result> DeletePoll(IUnitOfWork unitOfWork, IPollRepository pollRepository, int pollId)
+		{
+			Result<Poll> poll = await pollRepository.FindById(pollId);
+			if (!poll.IsSuccess) return poll;
 
-            return Result.CreateSuccess();
-        }
+			var removed = poll.Value.Delete();
 
-        public async Task<Result> DeletePoll(IUnitOfWork unitOfWork, IPollRepository pollRepository, int pollId)
-        {
-            Result<Poll> poll = await pollRepository.FindById(pollId);
-            if (!poll.IsSuccess) return poll;
+			pollRepository.Remove(removed);
+			await unitOfWork.SaveChanges();
+			return Result.CreateSuccess();
+		}
 
-            var removed = poll.Value.Delete();
-            
-            pollRepository.Remove(removed);
-            await unitOfWork.SaveChanges();
-            return Result.CreateSuccess();
-        }
+		public Task<Result<Poll>> FindById(IPollRepository pollRepository, int pollId)
+		{
+			return pollRepository.FindById(pollId);
+		}
 
-        public Task<Result<Poll>> FindById(IPollRepository pollRepository, int pollId)
-        {
-            return pollRepository.FindById(pollId);
-        }
+		public async Task<Result> Answer(IUnitOfWork unitOfWork, IPollRepository pollRepository, int pollId, int userId, int proposalId)
+		{
+			Result<Poll> pollResult = await FindById(pollRepository, pollId);
+			if (!pollResult.IsSuccess) return pollResult;
 
-        public async Task<Result> Answer(IUnitOfWork unitOfWork, IPollRepository pollRepository, int pollId, int userId, int proposalId)
-        {
-            Result<Poll> pollResult = await FindById(pollRepository, pollId);
-            if (!pollResult.IsSuccess) return pollResult;
+			Poll poll = pollResult.Value;
+			Result answerResult = poll.Answer(userId, proposalId);
+			if (!answerResult.IsSuccess) return answerResult;
 
-            Poll poll = pollResult.Value;
-            Result answerResult = poll.Answer(userId, proposalId);
-            if (!answerResult.IsSuccess) return answerResult;
+			await unitOfWork.SaveChanges();
 
-            await unitOfWork.SaveChanges();
+			return Result.CreateSuccess();
+		}
 
-            return Result.CreateSuccess();
-        }
+		public Task<Result<IEnumerable<Poll>>> FindUserPolls(IPollRepository pollRepository, int userId)
+		{
+			return pollRepository.FindUserPolls(userId);
+		}
 
-        public Task<Result<IEnumerable<Poll>>> FindUserPolls(IPollRepository pollRepository, int userId)
-        {
-            return pollRepository.FindUserPolls(userId);
-        }
+		public async Task<Result<Poll>> FindByIdForAuthor(IPollRepository pollRepository, int authorId, int pollId)
+		{
+			Result<Poll> poll = await pollRepository.FindById(pollId);
+			if (!poll.IsSuccess) return poll;
+			if (poll.Value.AuthorId != authorId) return Result.CreateError<Poll>(Errors.Forbidden, ForbiddenMsg);
+			return poll;
+		}
 
-        public async Task<Result<Poll>> FindByIdForAuthor(IPollRepository pollRepository, int authorId, int pollId)
-        {
-            Result<Poll> poll = await pollRepository.FindById(pollId);
-            if (!poll.IsSuccess) return poll;
-            if (poll.Value.AuthorId != authorId) return Result.CreateError<Poll>(Errors.Forbidden, ForbiddenMsg);
-            return poll;
-        }
+		public Task<Result<IEnumerable<Poll>>> FindUserInvitations(IPollRepository pollRepository, int userId)
+		{
+			return pollRepository.FindUserInvitations(userId);
+		}
 
-        public Task<Result<IEnumerable<Poll>>> FindUserInvitations(IPollRepository pollRepository, int userId)
-        {
-            return pollRepository.FindUserInvitations(userId);
-        }
+		public async Task<Result<Poll>> FindByIdForGuest(IPollRepository pollRepository, int pollId, int guestId)
+		{
+			Result<Poll> poll = await pollRepository.FindById(pollId);
+			if (!poll.IsSuccess) return poll;
+			if (!poll.Value.Guests.Any(g => g.UserId == guestId)) return Result.CreateError<Poll>(Errors.Forbidden, ForbiddenMsg);
+			return poll;
+		}
 
-        public async Task<Result<Poll>> FindByIdForGuest(IPollRepository pollRepository, int pollId, int guestId)
-        {
-            Result<Poll> poll = await pollRepository.FindById(pollId);
-            if (!poll.IsSuccess) return poll;
-            if (!poll.Value.Guests.Any(g => g.UserId == guestId)) return Result.CreateError<Poll>(Errors.Forbidden, ForbiddenMsg);
-            return poll;
-        }
-
-        public async Task<Result> DeleteGuest(IPollRepository pollRepository, int guestId, int pollId)
-        {
-            Result<Poll> poll = await pollRepository.FindById(pollId);
-            if (!poll.IsSuccess) return poll;
-            return poll.Value.RemoveGuest(guestId);
-        }
-    }
+		public async Task<Result> DeleteGuest(IPollRepository pollRepository, int guestId, int pollId)
+		{
+			Result<Poll> poll = await pollRepository.FindById(pollId);
+			if (!poll.IsSuccess) return poll;
+			return poll.Value.RemoveGuest(guestId);
+		}
+	}
 }
